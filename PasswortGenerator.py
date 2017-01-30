b6e05069cdb2335a613ccf835f0c69123d534952
KeVy0 <kevy0sec@gmail.com>
2017-01-30 21:56:14 +0100

    1.000.000 + Check. Und Shebang

--- PasswortGenerator.py
+++ PasswortGenerator.py
@@ -1,8 +1,9 @@
+#! /usr/bin/env python3
 import sys
 import string
 from random import *
 
-rsome = randint(1,20)
+rsome = randint(1,100)
 result = []
 
 
@@ -13,6 +14,18 @@ def usage():
 
 def random():
     userinput = int(sys.argv[1])
+    # Noch schnell reingebaut, Idioten und so
+    if userinput >= 1000000:
+        checkif100 = input("Passwort uber 1000000 Zeichen , bist du dir sicher ? (Kann langer dauern) [j/n] ~> ")
+        if checkif100 == "j":
+            print("Gut, du wolltest es so....")
+        elif checkif100 == "n":
+            print("Abbruch...")
+            exit()
+        else:
+            print("[j/n], sonst nichst")
+            random()
+     # Wenn Passwort über 1 Millionen wird nochmal nachgefragt       
     upperC = list(string.ascii_uppercase)
     lowerC = list(string.ascii_lowercase)
     digits = list(string.digits)
